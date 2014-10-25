cf663c16c9f5a6cf6e126bda5c4edde4c4f8be4b
Andreas Zecher <andreas@madebypixelate.com>
2014-10-25 19:40:06 +0200

    Updated nokogiri. Removed bundler. Removed deprecated tests.

--- Gemfile
+++ Gemfile
@@ -1,12 +1,11 @@
 source "http://rubygems.org"
 
-gem "nokogiri", ">= 1.5.3"
+gem "nokogiri", ">= 1.6.1"
 gem "redirect_follower", ">= 0.1.1"
 
 group :development do
   gem "shoulda", ">= 0"
-  gem "bundler", "~> 1.3.4"
+  gem "bundler", "~> 1.6.3"
   gem "jeweler", "~> 1.6.4"
-  gem "rcov", ">= 0"
   gem "test-unit", "~> 2.5.4"
 end
--- test/test_raev.rb
+++ test/test_raev.rb
@@ -1,8 +1,7 @@
 # encoding: UTF-8
 
 require 'helper'
-require 'raev/author'
-require 'raev/url'
+require 'raev'
 
 class TestRaev < Test::Unit::TestCase
   should "parse base url" do
@@ -32,12 +31,6 @@ class TestRaev < Test::Unit::TestCase
   end
   
   should "get twitter handle" do
-    url = Raev.url("http://www.polygon.com")
-    assert_equal "polygon", url.twitter
-  
-    url = Raev.url("http://penny-arcade.com/report")
-    assert_equal "thepareport", url.twitter
-  
     url = Raev.url("http://kotaku.com")
     assert_equal nil, url.twitter
   end
@@ -46,9 +39,6 @@ class TestRaev < Test::Unit::TestCase
     url = Raev.url("http://www.polygon.com")
     assert_equal "http://www.polygon.com/rss/index.xml", url.feed
     
-    url = Raev.url("http://penny-arcade.com/report")
-    assert_equal "http://feeds.penny-arcade.com/pa-report", url.feed
-  
     url = Raev.url("http://arstechnica.com")
     assert_equal "http://feeds.arstechnica.com/arstechnica/index/", url.feed
   
@@ -59,9 +49,6 @@ class TestRaev < Test::Unit::TestCase
   should "get headline from url" do
     url = Raev.url("http://www.polygon.com/e3-2013/2013/6/14/4429126/the-indie-eight-ps4")
     assert_equal "The Indie Eight: Polygon talks with the showcase indies launching on PS4", url.headline
-
-    url = Raev.url("http://penny-arcade.com/report/article/the-future-improved-hands-on-with-the-high-definition-oculus-rift-dev-kit")
-    assert_equal "The future, improved: hands on with the high-definition Oculus Rift dev kit", url.headline
 
     url = Raev.url("http://kotaku.com/the-world-of-a-link-to-the-past-has-changed-in-the-new-513424187")
     assert_equal "The World of A Link To The Past Has Changed in the New 3DS Zelda", url.headline
@@ -88,12 +75,6 @@ class TestRaev < Test::Unit::TestCase
     assert_equal "I Spy An Open Beta For Spy Party", url.headline
   end
     
-  should "get twitter and rss feed" do
-    url = Raev.url("http://www.polygon.com")
-    assert_equal "http://www.polygon.com/rss/index.xml", url.feed
-    assert_equal "polygon", url.twitter    
-  end
-  
   should "parse author from rss entry" do
     assert_equal "Andreas", Raev.normalize_author("andreas@somedomain.com (Andreas)")
     assert_equal "Andreas Zecher", Raev.normalize_author("Andreas \"Pixelate\" Zecher")
