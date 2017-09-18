3a9ab07665df199c9f52985448ebc50afc223827
Andreas Zecher <andreas@madebypixelate.com>
2017-09-18 09:51:02 +0200

    Updated gems and fixed tests.

--- Gemfile
+++ Gemfile
@@ -1,14 +1,14 @@
 source "http://rubygems.org"
 
-gem "json", '>= 1.8.3'
-gem "nokogiri", ">= 1.6.1"
+gem "json", '>= 2.1.0'
+gem "nokogiri", ">= 1.8.0"
 gem "redirect_follower", ">= 0.1.1"
 gem "sanitize", ">= 2.1.0"
-gem "chronic", ">=0.9.1"
+gem "chronic", ">=0.10.2"
 
 group :development do
   gem "shoulda", ">= 0"
-  gem "bundler", "~> 1.12.3"
+  gem "bundler", "~> 1.14.6"
   gem "jeweler", "2.1.1"
   gem "test-unit", "~> 2.5.4"
 end
--- test/test_url.rb
+++ test/test_url.rb
@@ -22,17 +22,17 @@ class UrlTest < Test::Unit::TestCase
   
   should "resolve url" do
     url = Raev.url("http://feedproxy.google.com/~r/fingergaming/~3/nBkNwBLq-U8/")
-    assert_equal "http://www.gamasutra.com/topic/smartphone-tablet/fg/2011/01/21/zynga-acquires-drop7-developer-areacode/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+fingergaming+%28FingerGaming%29", url.resolved
+    assert_equal "https://www.gamasutra.com/topic/smartphone-tablet/fg/2011/01/21/zynga-acquires-drop7-developer-areacode/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+fingergaming+%28FingerGaming%29", url.resolved
   end
   
   should "resolve and clean url" do 
     url = Raev.url("http://feedproxy.google.com/~r/fingergaming/~3/nBkNwBLq-U8/")
-    assert_equal "http://www.gamasutra.com/topic/smartphone-tablet/fg/2011/01/21/zynga-acquires-drop7-developer-areacode/", url.resolved_and_clean    
+    assert_equal "https://www.gamasutra.com/topic/smartphone-tablet/fg/2011/01/21/zynga-acquires-drop7-developer-areacode/", url.resolved_and_clean    
   end
   
   should "get twitter handle" do
-    url = Raev.url("http://polygon.com")
-    assert_equal nil, url.twitter
+    url = Raev.url("https://www.polygon.com")
+    assert_equal "Polygon", url.twitter
   end
   
   should "get rss feed" do
@@ -43,14 +43,14 @@ class UrlTest < Test::Unit::TestCase
     assert_equal "http://feeds.arstechnica.com/arstechnica/index/", url.feed
   
     url = Raev.url("http://www.kotaku.com")
-    assert_equal "http://feeds.gawker.com/kotaku/full", url.feed
+    assert_equal "https://kotaku.com/rss", url.feed
   end
   
   should "get headline from url" do
-    url = Raev.url("http://www.polygon.com/e3-2013/2013/6/14/4429126/the-indie-eight-ps4")
+    url = Raev.url("https://www.polygon.com/e3-2013/2013/6/14/4429126/the-indie-eight-ps4")
     assert_equal "The Indie Eight: Polygon talks with the showcase indies launching on PS4", url.headline
 
-    url = Raev.url("http://kotaku.com/the-world-of-a-link-to-the-past-has-changed-in-the-new-513424187")
+    url = Raev.url("https://kotaku.com/the-world-of-a-link-to-the-past-has-changed-in-the-new-513424187")
     assert_equal "The World of A Link To The Past Has Changed in the New 3DS Zelda", url.headline
     
     url = Raev.url("http://arstechnica.com/gaming/2012/03/journey-a-hauntingly-beautiful-art-house-film-disguised-as-a-game/")
@@ -65,7 +65,7 @@ class UrlTest < Test::Unit::TestCase
     url = Raev.url("http://indiegames.com/2013/06/indie_fund_backing_for_two_new.html")
     assert_equal "Indie Fund backing two new titles for Double Fine", url.headline
 
-    url = Raev.url("https://killscreendaily.com/articles/news/cheat-sheet-614/")
+    url = Raev.url("https://killscreen.com/articles/news/cheat-sheet-614/")
     assert_equal "Pixels on canvas, Spielberg's predictions, and Polytron's glorious tease - Kill Screen", url.headline
 
     url = Raev.url("https://www.rockpapershotgun.com/2013/06/05/i-spy-an-open-beta-for-spy-party/")
@@ -73,20 +73,17 @@ class UrlTest < Test::Unit::TestCase
   end
   
   should "get pubdate from url" do
-    url = Raev.url("http://www.polygon.com/2015/5/18/8620223/witcher-3-guide-witcher-2-witcher")
+    url = Raev.url("https://www.polygon.com/2015/5/18/8620223/witcher-3-guide-witcher-2-witcher")
     assert_equal_date Date.new(2015, 5, 18), url.pubdate
     
-    url = Raev.url("http://kotaku.com/this-week-destiny-got-a-hell-of-a-lot-better-1706391634")
+    url = Raev.url("https://kotaku.com/this-week-destiny-got-a-hell-of-a-lot-better-1706391634")
     assert_equal_date Date.new(2015, 5, 23), url.pubdate
     
     url = Raev.url("https://www.rockpapershotgun.com/2014/07/03/beauty-beheld-future-unfolding/")
     assert_equal_date Date.new(2014, 7, 3), url.pubdate
     
-    url = Raev.url("http://jayisgames.com/review/the-black-forest-finding-friends.php")
+    url = Raev.url("https://jayisgames.com/review/the-black-forest-finding-friends.php")
     assert_equal_date Date.new(2009, 12, 9), url.pubdate
-    
-    url = Raev.url("http://boingboing.net/2007/03/21/understanding-games.html")
-    assert_equal_date Date.new(2007, 3, 21), url.pubdate
     
     url = Raev.url("http://www.wired.com/2014/09/upcoming-a-gorgeous-adventure-game-that-mutates-for-each-player/")
     assert_equal_date Date.new(2014, 9, 4), url.pubdate
@@ -102,17 +99,17 @@ class UrlTest < Test::Unit::TestCase
     url = Raev.url("http://www.polygon.com/features/2013/3/25/4128022/gdc-gathering-of-game-makers")
     assert_equal "Colin Campbell", url.author
     
-    url = Raev.url("http://kotaku.com/worth-reading-some-kickstarters-are-lying-about-game-b-1706340013")
+    url = Raev.url("https://kotaku.com/worth-reading-some-kickstarters-are-lying-about-game-b-1706340013")
     assert_equal "Patrick Klepek", url.author
     
-    url = Raev.url("https://killscreendaily.com/articles/future-unfolding-wonder/")
+    url = Raev.url("https://killscreen.com/articles/future-unfolding-wonder/")
     assert_equal "Jess Joho", url.author
     
     url = Raev.url("http://www.creativeapplications.net/games/future-unfolding-procedurally-generated-world-both-beautiful-and-dangerous/")
     assert_equal "Filip Visnjic", Raev::normalize_author(url.author)
 		
 		url = Raev.url("https://www.youtube.com/watch?v=FmZYPMsq5m4")
-		assert_equal "PsiSyndicate", url.author
+		assert_equal "PsiSyn", url.author
   end
 	
 	should "get score" do
